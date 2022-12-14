279413ee4e6661729b6a678ff14d36a54c5281c3
George Alan <georgealanrufo@gmail.com>
2022-12-14 19:15:26 -0300

    Training streams

--- Exercices/Streams/src/test/java/examples/GroupingData.java
+++ Exercices/Streams/src/test/java/examples/GroupingData.java
@@ -6,12 +6,26 @@ import mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class GroupingData {
 
     @Test
     public void simpleGrouping() throws Exception {
-        List<Car> cars = MockData.getCars();
+        Map<String, List<Car>> carMakers =  MockData.getCars()
+                .stream()
+                .collect(Collectors.groupingBy(Car::getMake));
+
+        carMakers.forEach((maker, cars) -> {
+            System.out.println("Maker: " + maker);
+            cars.forEach(System.out::println);
+            System.out.println("--------------------------");
+        });
+        /*
+        Here you can use groupBy like use in SQL for group results in a organized bunch.
+         */
     }
 
     @Test
@@ -27,6 +41,18 @@ public class GroupingData {
                 "Alex",
                 "Alex"
         );
+
+        Map<String, Long> namesMap = names.stream()
+                .collect(Collectors.groupingBy(name -> name, Collectors.counting()));
+
+        Map<String, Long> namesMapOtherApproach = names.stream()
+                        .collect(Collectors.groupingBy(
+                                Function.identity(),
+                                Collectors.counting())
+                        );
+
+        System.out.println(namesMap);
+        System.out.println(namesMapOtherApproach);
     }
 
 }
--- Exercices/Streams/src/test/java/examples/StatisticsWithStreams.java
+++ Exercices/Streams/src/test/java/examples/StatisticsWithStreams.java
@@ -5,6 +5,8 @@ import com.george.beans.Car;
 import mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
 public class StatisticsWithStreams {
@@ -26,27 +28,51 @@ public class StatisticsWithStreams {
                 .mapToDouble(Car::getPrice)
                 .min()
                 .orElse(0);
+        System.out.println(mostCheapCar);
     }
 
     @Test
     public void max() throws Exception {
         List<Car> cars = MockData.getCars();
+        double mostExpensiveCar = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .max()
+                .orElse(0);
+        System.out.println(mostExpensiveCar);
     }
 
 
     @Test
     public void average() throws Exception {
         List<Car> cars = MockData.getCars();
+        double average = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .average()
+                .orElse(0);
+        System.out.println(average);
     }
 
     @Test
     public void sum() throws Exception {
         List<Car> cars = MockData.getCars();
+        double sum = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .sum();
+        System.out.println(BigDecimal.valueOf(sum));
     }
 
     @Test
     public void statistics() throws Exception {
         List<Car> cars = MockData.getCars();
+        DoubleSummaryStatistics statistics = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .summaryStatistics();
+
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Average price: " + statistics.getAverage());
+        System.out.println("Minimum price: " + statistics.getMin());
+        System.out.println("Maximum price: " + statistics.getMax());
+        System.out.println("Sum of all prices: " + BigDecimal.valueOf(statistics.getSum()));
     }
 
 }
