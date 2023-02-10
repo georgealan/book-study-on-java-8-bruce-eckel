1053775a7c8a5b6a073f2ed0a6e1d22ecbc6834a
George Alan <georgealanrufo@gmail.com>
2023-02-09 22:09:38 -0300

    Practice Java Streams

--- Exercices/Streams/src/test/java/examples/JoiningStrings.java
+++ Exercices/Streams/src/test/java/examples/JoiningStrings.java
@@ -11,13 +11,28 @@ public class JoiningStrings {
     public void joiningStrings() throws Exception {
         List<String> names = List.of("anna", "john", "marcos", "helena", "yasmin");
         // "Anna, John, Marcos, Helena, Yasmin"
+        StringBuilder join = new StringBuilder();
+
+        for (String name : names) {
+            join.append(name.substring(0, 1).toUpperCase())
+                    .append(name.substring(1))
+                    .append(", ");
+        }
+        join.deleteCharAt(join.length() - 2);
+
+//        System.out.println(join.substring(0, join.length() - 2));
+        System.out.println(join);
     }
 
     @Test
     public void joiningStringsWithStream() throws Exception {
         List<String> names = List.of("anna", "john", "marcos", "helena", "yasmin");
         // "Anna, John, Marcos, Helena, Yasmin"
+        String join = names.stream()
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .collect(Collectors.joining(", "));
 
+        System.out.println(join);
     }
 
 }
--- Exercices/Streams/src/test/java/examples/Sorting.java
+++ Exercices/Streams/src/test/java/examples/Sorting.java
@@ -6,28 +6,68 @@ import mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Sorting {
 
     @Test
     public void sortingSteamOfElements() throws IOException {
         List<Person> people = MockData.getPeople();
+        // Sort person by name in alphabetical order.
+        List<String> personSorted = people.stream()
+                .map(Person::getFirstName)
+                .sorted()
+                .collect(Collectors.toList());
+
+        personSorted.forEach(System.out::println);
     }
 
     @Test
     public void sortingSteamOfElementsReverse() throws IOException {
         List<Person> people = MockData.getPeople();
+        // Sort person by name in alphabetical reverse order
+        List<String> personSorted = people.stream()
+                .map(Person::getFirstName)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        personSorted.forEach(System.out::println);
     }
 
     @Test
     public void sortingSteamOfObjets() throws IOException {
         List<Person> people = MockData.getPeople();
+        // Sort without using map()
+        List<Person> peopleSorted = people.stream()
+                .sorted(Comparator.comparing(Person::getFirstName))
+                .collect(Collectors.toList());
+
+        List<Person> peopleSortedReverse = people.stream()
+                .sorted(Comparator.comparing(Person::getFirstName).reversed())
+                .collect(Collectors.toList());
+
+        List<Person> peopleSortedByTwoParams = people.stream()
+                .sorted(Comparator.comparing(Person::getFirstName)
+                        .thenComparing(Person::getEmail))
+                .collect(Collectors.toList());
+
+//        peopleSorted.forEach(System.out::println);
+//        peopleSortedReverse.forEach(System.out::println);
+        peopleSortedByTwoParams.forEach(System.out::println);
     }
 
     @Test
     public void topTenMostExpensiveBlueCars() throws IOException {
         List<Car> cars = MockData.getCars();
+        List<Car> topTenMostExpensiveCars = cars.stream()
+                .filter(car -> car.getColor().equalsIgnoreCase("blue"))
+                .sorted(Comparator.comparing(Car::getPrice).reversed())
+                .limit(10)
+                .collect(Collectors.toList());
+
+        topTenMostExpensiveCars.forEach(System.out::println);
     }
 
 }
